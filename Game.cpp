4ac3b8b5f1fd9e2cdb91e7e049ebafdd54107ca7
DavisKoreal <daviskipchirchir19@gmail.com>
2023-06-12 23:35:18 +0300

    abstract implementation

---
+++ Game.cpp
@@ -0,0 +1,2 @@
+#include "projectmodules.hpp"
+
---
+++ projectmodules.hpp
@@ -0,0 +1,17 @@
+
+
+#ifndef PROJECTMODULES_HPP
+#define PROJECTMODULES_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <fstream>
+#include "Game.hpp"
+
+#endif
